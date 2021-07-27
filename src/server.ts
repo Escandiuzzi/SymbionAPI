445d57851806de4968df3b213b6f2a930fc98bde
Luiz Felipe Escandiuzzi <luiz.escandiuzzi@duo.studio>
2021-07-26 22:49:45 -0300

    Added typeorm

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+
+import "./database"
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
